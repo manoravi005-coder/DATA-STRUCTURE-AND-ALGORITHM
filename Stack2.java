c6dcba82795bcbf705118d89668b8742347ec2ca
Manobala.R <manoravi005@gmail.com>
2026-08-04 22:06:10 +0530

    Create Stack2.java

---
+++ Stack2.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Stack<String> stack = new Stack<String>();
+		stack.push("Minecraft");
+		stack.push("james");
+		stack.push("bond");
+		stack.push("looser");
+		stack.pop();
+		stack.pop();
+		stack.pop();
+		System.out.println(stack);
+
+	}
+}
