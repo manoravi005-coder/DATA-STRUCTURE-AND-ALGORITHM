f7098993b618e45c6d5d60c5a2366f3a4f4b70e8
Manobala.R <manoravi005@gmail.com>
2026-08-09 21:44:31 +0530

    Create Linked List3(singly).java

---
+++ Linked List3(singly).java
@@ -0,0 +1,39 @@
+import java.util.*;
+class Node{
+  int data;
+  Node next;
+  Node(int data){
+    this.data = data;
+    this.next = null;
+  }
+}
+public class Main{
+  public static void main(String[] args){
+    Scanner sc = new Scanner(System.in);
+    int n = sc.nextInt();
+    Node head = null;
+    Node tail = null;
+    for(int i =0; i<n; i++){
+      int val = sc.nextInt();
+      Node newnode = new Node(val);
+      if(head == null){
+        head = newnode;
+        tail = newnode;
+      }
+      else{
+        tail.next = newnode;
+        tail = newnode;
+       
+      }
+    }
+    Node current = head;
+    boolean found = false;
+    int target = sc.nextInt();
+    while(current != null){
+      if(current.data == target) found = true;
+      current = current.next;
+    }
+    if(found) System.out.print("Element found");
+    else System.out.print("Element not found");
+  }
+}
